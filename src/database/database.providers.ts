f9ba2877f80635dc42ba3c48138db7a0f809f528
Nikita Kozlov <nk@bankexfoundation.org>
2018-07-05 20:57:21 +0300

    make code better

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,21 +4,11 @@ import {config} from 'dotenv';
 config();
 
 const local = 'mongodb://localhost:27017';
-const prod = `mongodb://${(process.env.LOGIN)}:${(process.env.PASSWORD)}@${(process.env.URL_CONNECTION)}/${(process.env.DB)}`;
+const env = process.env;
+const prod = `mongodb://${env.LOGIN}:${env.PASSWORD}@${env.URL_CONNECTION}/${env.DB}`;
 
-function getMongoProviderConfig(): string{
-    if (process.env.CONFIG === 'prod') {
-        console.log('using prod DB mongo');
-        return prod;
-    }
-    else if (process.env.CONFIG === 'local') {
-        return local;
-    }
-
-    else {
-        console.log('Using localhost DB by Default, if you want to use prod, please add CONFIG=prod to npm start');
-        return local;
-    }
+function getMongoProviderConfig(): string {
+    return env.CONFIG === 'prod' ? prod : local;
 }
 
 export const databaseProviders = [
