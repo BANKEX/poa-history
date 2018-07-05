d8ca91b3b02d10122fbc143ae530c1de141f74d5
Nikita Kozlov <nk@bankexfoundation.org>
2018-07-05 20:49:01 +0300

    add config settings

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,11 +1,31 @@
 import * as mongoose from 'mongoose';
 import {config} from 'dotenv';
+
 config();
+
+const local = 'mongodb://localhost:27017';
+const prod = `mongodb://${(process.env.LOGIN)}:${(process.env.PASSWORD)}@${(process.env.URL_CONNECTION)}/${(process.env.DB)}`;
+
+function getMongoProviderConfig(): string{
+    if (process.env.CONFIG === 'prod') {
+        console.log('using prod DB mongo');
+        return prod;
+    }
+    else if (process.env.CONFIG === 'local') {
+        return local;
+    }
+
+    else {
+        console.log('Using localhost DB by Default, if you want to use prod, please add CONFIG=prod to npm start');
+        return local;
+    }
+}
 
 export const databaseProviders = [
     {
         provide: 'DbConnectionToken',
         useFactory: async (): Promise<typeof mongoose> =>
-            await mongoose.connect(`mongodb://${(process.env.LOGIN)}:${(process.env.PASSWORD)}@${(process.env.URL_CONNECTION)}/${(process.env.DB)}`),
+            await mongoose.connect(getMongoProviderConfig()),
     },
 ];
+
