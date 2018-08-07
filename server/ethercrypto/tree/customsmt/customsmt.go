a0b50eaaeb7ed95a9bace8c746d0b0b0f10f4852
Nikita Kozlov <nk@bankexfoundation.org>
2018-08-07 09:54:52 -0400

    add stable

--- server/ethercrypto/tree/customsmt/customsmt.go
+++ server/ethercrypto/tree/customsmt/customsmt.go
@@ -2,7 +2,7 @@ package customsmt
 
 import (
 	"log"
-	"../merkletree"
+	"../smerkletree"
 	"github.com/miguelmota/go-solidity-sha3"
 )
 
@@ -20,37 +20,37 @@ func (t TestContent) CalculateHashBytes() ([]byte, error) {
 }
 
 //Equals tests for equality of two Contents
-func (t TestContent) Equals(other merkletree.Content) (bool, error) {
+func (t TestContent) Equals(other smerkletree.Content) (bool, error) {
 	return t.X == other.(TestContent).X, nil
 }
 
-func CreateContent(content []string) []merkletree.Content {
-	var list []merkletree.Content
+func CreateContent(content []string) []smerkletree.Content {
+	var list []smerkletree.Content
 	for i := 0; i < len(content); i++ {
 		list = append(list, TestContent{X: content[i]})
 	}
 	return list
 }
 
-func CreateTree(list []merkletree.Content) *merkletree.MerkleTree {
+func CreateTree(list []smerkletree.Content) *smerkletree.MerkleTree {
 	//Create a new Merkle Tree from the list of Content
-	t, err := merkletree.NewTree(list)
+	t, err := smerkletree.NewTree(list)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return t
 }
 
-func RewriteTree(content []merkletree.Content, tree *merkletree.MerkleTree) {
+func RewriteTree(content []smerkletree.Content, tree *smerkletree.MerkleTree) {
 	tree.RebuildTreeWith(content)
 	//tree.RebuildTree()
 }
 
-func GetMerkleRoot(tree *merkletree.MerkleTree) []byte {
+func GetMerkleRoot(tree *smerkletree.MerkleTree) []byte {
 	return tree.MerkleRoot()
 }
 
-func VerifySpecificLeaf(tree *merkletree.MerkleTree, content merkletree.Content) bool {
+func VerifySpecificLeaf(tree *smerkletree.MerkleTree, content smerkletree.Content) bool {
 	res, err := tree.VerifyContent(content)
 	if err != nil {
 		log.Fatal(err)
@@ -58,11 +58,11 @@ func VerifySpecificLeaf(tree *merkletree.MerkleTree, content merkletree.Content)
 	return res
 }
 
-func ShowLeafs(tree *merkletree.MerkleTree) string {
+func ShowLeafs(tree *smerkletree.MerkleTree) string {
 	return tree.String()
 }
 
-func VerifyAll(tree *merkletree.MerkleTree) bool {
+func VerifyAll(tree *smerkletree.MerkleTree) bool {
 	res, err := tree.VerifyTree()
 	if err != nil {
 		log.Fatal(err)
@@ -70,6 +70,6 @@ func VerifyAll(tree *merkletree.MerkleTree) bool {
 	return res
 }
 
-func Hashes(tree *merkletree.MerkleTree) []string {
+func Hashes(tree *smerkletree.MerkleTree) []string {
 	return tree.GetHash()
 }
