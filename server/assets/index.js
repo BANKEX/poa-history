b3deddcf86a01b70fd5b58a6458ce4109a2eed70
unknown <krboktv@gmail.com>
2018-08-28 23:37:48 +0300

    Add Merkle verify algorithm

--- server/assets/index.js
+++ server/assets/index.js
@@ -1,55 +1,71 @@
-function getData(assetId, txNumber, hash) {
-    var xmlHttp = new XMLHttpRequest();
-    // xmlHttp.open("GET", "/proof"+"/"+ assetId +"/"+ txNumber +"/"+ hash, false); // false for synchronous request
-    a = "a";
-    b = "0";
-    c = "96e75810b7fe519dd92f6a3f72170b00c0a8a9553f9c765a3cc681eaf7eeab38";
-    xmlHttp.open("GET", "/proof/" + a + "/" + b + "/" + c, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
-
-function sort() {
-    var data = getData(1, 1, 1);
-    var s = JSON.parse(data);
-
-    // var group = []
-    //
-    // for (var i = 0; i < s.length; i++) {
-    //     group.push(s[i].Number)
-    // }
-    //
-    // var d = groupn(group)
-    // while (d.length > 1) {
-    //     d = groupn(d)
-    //     // console.log("t")
-    // }
-
-    return s
-}
-
 const left = 0;
 const right = 1;
 
-function verifyProof(data, key, hash, root) {
-    keyHash = getHash(key);
-    dataHash = getHash(hash);
-
-    for (let i = 255; i >= 0; i--) {
-        var node = [255];
-        node[i] = (data[i]);
-        if (hasBit(path, i) == right) {
-            dataHash = getHash(node.concat(dataHash))
-
-        } else {
-            dataHash = getHash(dataHash.concat(node))
-        }
-    }
-    return equal(dataHash, root)
+function verify(assetId, txNumber, data, timestamp) {
+    const response = getData(assetId, txNumber, data, timestamp);
+    const proof = response.Data;
+    const key = Base64toHEX(response.Info.Key);
+    const hash = Base64toHEX(response.Info.Hash);
+    const root = Base64toHEX(response.Info.Root);
+    const verify = verifyProof(proof, key, hash, root);
+    return verify;
 }
 
-function hasBit(data, position) {
-    if ((data[position / 8]) & (1 << (uint(position) % 8)) > 0) {
+/**
+ * Allows to get Merkle proofs
+ * @param assetId {string} ID of verifiable asset
+ * @param txNumber {string} Number of transaction
+ * @param data {string} Verifiable data
+ * @param timestamp {string} Time of adding data
+ * @returns {Object}
+ */
+function getData(assetId, txNumber, data, timestamp) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "/proof"+"/"+ assetId +"/"+ txNumber +"/"+ data + "/" + timestamp, false); // false for synchronous request
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
+}
+
+/**
+ * Allows to get varify of proof
+ * @param proof {Array} 256 length array of nodes
+ * @param key {string} Data key
+ * @param data {string} Verifiable data
+ * @param root {string} Merkle root hash
+ * @returns {boolean} result of verifying
+ */
+function verifyProof(proof, key, data, root) {
+    const rootHash = HexToUint8Array(root);
+    const keyHash = getHash("0x"+key);
+    let dataHash = getHash("0x"+data);
+
+    if (data.length != 256)
+        return false;
+
+    for (let i = 255; i >= 0; i--) {
+        const node = Base64ToBinary(proof[i].Hash);
+        if (node.length != 32)
+            return false;
+        let newArray;
+        if (hasBit(keyHash, i) == right)
+            newArray = concatUint8Arrays(node, dataHash);
+        else
+            newArray = concatUint8Arrays(dataHash, node);
+        const newHex = "0x" + Uint8ArrayToHex(newArray);
+        dataHash = getHash(newHex);
+    }
+
+    return Uint8ArrayToHex(dataHash) === Uint8ArrayToHex(rootHash);
+}
+
+/**
+ * Allows to find adjacent node
+ * @param key Key value
+ * @param position Current position
+ * @returns {number} Left - 0, Right - 1
+ */
+function hasBit(key, position) {
+    if (((key[parseInt(position / 8)]) & (1 << (position % 8))) > 0) {
         return 1
     }
     return 0
@@ -58,17 +74,77 @@ function hasBit(data, position) {
 /**
  * Allows to hashing data via keccak256
  * @param data Any set of data
- * @return {*} Hash of data
+ * @return {Uint8Array} Hash of data
  */
 function getHash(data) {
-    const web3 = new Web3();
-    return web3.utils.keccak256(data);
+    const hash_hex = web3.utils.keccak256(data).substring(2);
+    const hash_bytes = HexToUint8Array(hash_hex);
+    return hash_bytes;
 }
 
-function tryN() {
-    var s = sort()
-    key = "d298be85487e0f453736b6b4a5634d09cc08790493e03941a699d0444d1ee1e4";
-    hash = "96e75810b7fe519dd92f6a3f72170b00c0a8a9553f9c765a3cc681eaf7eeab38";
-    root = "4101c0157600045fdf8dcb8e8a78c5891606bd596a7c497b5e9091ca08f9dc6d";
-    verifyProof(s, key, hash, root)
+/**
+ * Allows to convert base64 string into hex string
+ * @param base64 {string} base64 data
+ * @returns {string} hex data
+ * @constructor
+ */
+function Base64toHEX(base64) {
+    const raw = window.atob(base64);
+    var HEX = '';
+    for (i = 0; i < raw.length; i++) {
+        var _hex = raw.charCodeAt(i).toString(16)
+        HEX += (_hex.length == 2 ? _hex : '0' + _hex);
+    }
+    return HEX.toLowerCase();
+}
+
+/**
+ * Allows to convert base64 string into Uint8Array
+ * @param data {string} base64
+ * @returns {Uint8Array}
+ */
+function Base64ToBinary(data) {
+    const raw = window.atob(data);
+    const rawLength = raw.length;
+    const array = new Uint8Array(new ArrayBuffer(rawLength));
+    for (let i = 0; i < rawLength; i++)
+        array[i] = raw.charCodeAt(i);
+    return array;
+}
+
+/**
+ * Allows to convert Uint8Array into Hex
+ * @param array Uint8Array
+ * @returns {string} Hex data
+ */
+function Uint8ArrayToHex(array) {
+    return array.reduce(function (memo, i) {
+        return memo + ('0' + i.toString(16)).slice(-2); //padd with leading 0 if <16
+    }, '');
+}
+
+/**
+ * Allows to convert hex data into Uint8Array
+ * @param hexString Hex data
+ * @returns {Uint8Array}
+ */
+function HexToUint8Array(hexString) {
+    return new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+}
+
+/**
+ * Allows to join two arrays
+ * @param a First array
+ * @param b Second array
+ * @returns {Uint8Array}
+ */
+function concatUint8Arrays(a, b) {
+    const arr64 = new Uint8Array(a.length + b.length);
+    for (let i = 0; i < 64; i++) {
+        if (i < 32)
+            arr64[i] = a[i];
+        else
+            arr64[i] = b[i - 32];
+    }
+    return arr64;
 }
