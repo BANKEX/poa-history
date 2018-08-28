2a9696bd48553189031fe253ad3a7169ae7bdaf0
unknown <krboktv@gmail.com>
2018-08-29 00:00:08 +0300

    Add timestamp to verify func

--- server/assets/index.js
+++ server/assets/index.js
@@ -21,9 +21,9 @@ function verify(assetId, txNumber, data, timestamp) {
  * @param timestamp {string} Time of adding data
  * @returns {Object}
  */
-function getData(assetId, txNumber, data) {
+function getData(assetId, txNumber, data, timestamp) {
     const xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "/proof"+"/"+ assetId +"/"+ txNumber +"/"+ data, false); // false for synchronous request
+    xmlHttp.open("GET", "/proof"+"/"+ assetId +"/"+ txNumber +"/"+ data + "/" + timestamp, false); // false for synchronous request
     xmlHttp.send(null);
     return JSON.parse(xmlHttp.responseText);
 }
