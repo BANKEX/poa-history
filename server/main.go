373372128d79ce6288c52f3df6ea6dcc7fb9a7de
EnoRage <nk@bankexfoundation.org>
2018-08-29 18:24:19 +0300

    some fixes

--- server/main.go
+++ server/main.go
@@ -34,5 +34,10 @@ func main() {
 	r.Static("/assets/main", "./assets/main")
 	r.Static("/assets/upload", "./assets/upload")
 	r.Static("/assets/download", "./assets/download")
+
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(301, "http://ec2-18-210-150-89.compute-1.amazonaws.com:80/assets/main/")
+	})
+
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
