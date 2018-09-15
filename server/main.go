56b17f4847a2970542cc16f3b2601c2e677a7178
EnoRage <nk@bankexfoundation.org>
2018-09-15 19:54:33 +0300

    add fixes

--- server/main.go
+++ server/main.go
@@ -27,7 +27,7 @@ func main() {
 	// @license.name MIT
 	// @license.url https://opensource.org/licenses/MIT
 
-	// @host localhost:8080
+	// @host history.bankex.team:8080
 	// @BasePath /
 
 	db.Connect()
